67e8b9e424458d80de837b1375bbf100990d3ff8
Sckab <giulianodeamicis2009@gmail.com>
2025-05-23 18:42:00 +0200

    fixed folding and i hope fixed indenting

--- lua/opts.lua
+++ lua/opts.lua
@@ -7,7 +7,7 @@ vim.opt.tabstop = 3
 vim.opt.softtabstop = 3
 vim.opt.shiftwidth = 3
 vim.opt.expandtab = true
-vim.smartindent = true
+vim.opt.smartindent = true 
 
 vim.opt.wrap = false
 
@@ -17,3 +17,16 @@ vim.opt.incsearch = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldenable = true
+vim.opt.foldlevel = 99
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  callback = function()
+    vim.opt.foldmethod = "expr"
+    vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    vim.opt.foldlevel = 99
+  end,
+})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,11 +4,26 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "python", "javascript", "typescript", "html", "css", "c", "cpp", "c_sharp" },
+        ensure_installed = {
+          "lua", "python", "javascript", "typescript", "html", "css",
+          "c", "cpp", "c_sharp"
+        },
         highlight = { enable = true },
-        fold = { enable = true },
         indent = { enable = true },
+        fold = {
+          enable = true,
+        },
+
+        playground = {
+          enable = true,
+          updatetime = 25,
+          persist_queries = false,
+        },
       })
     end,
+  },
+  {
+    "nvim-treesitter/playground",
+    cmd = "TSPlaygroundToggle",
   },
 }
