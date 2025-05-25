d8908b752de7838cd42daf0b5bc7e23f2d7c553f
Sckab <giulianodeamicis2009@gmail.com>
2025-05-25 16:51:55 +0200

    modified accept key for codeium, now is tab

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -3,8 +3,8 @@ return {
   config = function()
     vim.cmd([[
       let g:codeium_enabled = 1
-      imap <silent><script><expr> <C-e> codeium#Accept()
       let g:codeium_no_map_tab = 1
+      imap <silent><script><expr> <Tab> codeium#Accept()
     ]])
   end,
 }
