3513e18e1a27822eb117794083f92257f918fd29
Sckab <giulianodeamicis2009@gmail.com>
2025-06-01 22:17:13 +0200

    changed the tabs: 3 spaces -> 4 spaces

--- lua/opts.lua
+++ lua/opts.lua
@@ -3,13 +3,13 @@ vim.g.mapleader = " "
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 3
-vim.opt.softtabstop = 3
-vim.opt.shiftwidth = 3
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true 
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
