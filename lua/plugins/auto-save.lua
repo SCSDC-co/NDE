1137a5f29af5281859cc26ad054d7d3825a1aea1
Sckab <giulianodeamicis2009@gmail.com>
2025-06-10 20:49:37 +0200

    re-added auto save

---
+++ lua/plugins/auto-save.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"Pocco81/auto-save.nvim",
+		config = function()
+			require("auto-save").setup({
+				trigger_events = { "InsertLeave" },
+				execution_message = {
+					message = function()
+						return ("💾 Auto Save: " .. vim.fn.strftime("%H:%M:%S"))
+					end,
+				},
+			})
+		end,
+	},
+}
